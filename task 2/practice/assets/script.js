2e8ba3f9250942bc098185e33f5e964c3a3a6c17
PriyankaRoteSuccessive <priyanka.rote@successive.tech>
2021-11-10 18:48:09 -0500

    completed

--- task 2/practice/assets/script.js
+++ task 2/practice/assets/script.js
@@ -8,12 +8,79 @@ console.log(`with variable ${a == b}`);
 let c = 'abc';  
 let d = 'abc';  
 // console.log(c == d); 
-console.log(`with let ${c == d}`); 
+console.log(`with let ${c == d}`); //templet string
 
 //biginit
 
-//comparision operater
+let e=117823928394820384239n;
+console.log(`Bigint ${e}`);
+console.log(typeof(e));
 
-//assigiment operater
+//Operators
 
-//function
+//Arithmetic Operators
+
+let f = 10;
+let g = 5;
+let h = f + g;
+console.log(`Addition ${h}`);
+
+let i = f - g;
+console.log(`Substraction ${i}`);
+
+let j = f * g;
+console.log(`Multipication ${j}`);
+
+let k = f ** g;
+console.log(`Exponentiation ${k}`);
+
+let l = f / g;
+console.log(`Division ${l}`);
+
+let m = f % g;
+console.log(`Modulus (Division Remainder) ${m}`);
+
+ f ++ ;
+ console.log(`Increment ${f}`);
+
+ g--;
+console.log(`Decrement ${g}`);
+
+//Assignment Operators
+let n = 15 ;
+console.log(`= operater ${n}`);
+
+n += 5;
+console.log(`+= operater ${n}`);
+
+n -= 5;
+console.log(`-= operater ${n}`);
+
+n *= 5;
+console.log(`*= operater ${n}`);
+
+n /= 5;
+console.log(`/= operater ${n}`);
+
+n %= 5;
+console.log(`%= operater ${n}`);
+
+//Comparison Operators
+o = 2;
+console.log(`== operater ${o == 2}`);
+console.log(`== operater ${o == 15}`);
+console.log(`==" " operater ${o == "25"}`);
+console.log(`=== operater ${o === 2}`);
+console.log(`===" " operater ${o === "2"}`);
+console.log(`!= operater ${o != 5}`);
+console.log(`!= operater ${o != 2}`);
+console.log(`!== " " operater ${o !== "2"}`);
+console.log(`!== operater ${o !== 8}`);
+console.log(`> operater ${o > 8}`);
+console.log(`< operater ${o < 8 }`);
+console.log(`>= operater ${o >= 8}`);
+console.log(`<= operater ${o <= 8}`);
+
+
+
+
