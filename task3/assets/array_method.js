75b724cd5ddc79fbad2b559b8bec47edd92688db
PriyankaRoteSuccessive <priyanka.rote@successive.tech>
2021-11-15 12:28:58 -0500

    completed

--- task3/assets/array_method.js
+++ task3/assets/array_method.js
@@ -1,65 +1,63 @@
 const array = [100,200,300,400,500,600,700,800,900]
-const y = ["d","b","a","c","f","e",]
+const y = ["d","b","a","c","f","e"]
 
-// // 1 length
-// console.log(`Length : => `+array.length);
+// 1 length
+console.log(`Length : => `+array.length);
 
-// // 2 indexof
-// console.log(`IndexOf : => `+array.indexOf(500));
+// 2 indexof
+console.log(`IndexOf : => `+array.indexOf(500));
 
-// // 3 include()
-//  console.log(`include t : => `+array.includes(500));
+// 3 include()
+ console.log(`include t : => `+array.includes(500));
+ console.log(`Include f : => `+array.includes(150));
 
-//  console.log(`Include f : => `+array.includes(150));
+// 4 sort()
+console.log(`array() : => `+ y);
+y.sort();
+ console.log(`sort() : => `+ y);
 
-// // 4 sort()
-// console.log(`array() : => `+ y);
+ // 5 reverse
+ y.reverse();
+ console.log(`reverse() : => `+ y);
 
-// y.sort();
-//  console.log(`sort() : => `+ y);
+ // 6 join()
+ console.log(`join() : => `+ y.join(" my alphabet "));
 
-//  // 5 reverse
-//  y.reverse();
-//  console.log(`reverse() : => `+ y);
+ // 7 pop()
+ array.pop();
+ console.log(`pop() : => `+ array);
 
-//  // 6 join()
-//  console.log(`join() : => `+ y.join(" my alphabet "));
+  // 8 push()
+  array.push(150);
+  console.log(`push() : => `+ array);
 
-//  // 7 pop()
-//  array.pop();
-//  console.log(`pop() : => `+ array);
+// 9 slice() it out element start with given index
+const z = array.slice(4);
+console.log(`slice : => `+ z );
 
-//   // 8 push()
-//   array.push(150);
-//   console.log(`push() : => `+ array);
+// 10 shift()
+array.shift();
+console.log(`shift() : => `+array);
 
-// // 9 slice() it out element start with given index
-// const z = array.slice(4);
-// console.log(`slice : => `+ z );
+// 11 unshift()
+array.unshift(50);
+console.log(`unshift() : => `+array);
 
-// // 10 shift()
-// array.shift();
-// console.log(`shift() : => `+array);
+//12 index number
+array[1] = 90;
+console.log(`index number() : => `+array);
 
-// // 11 unshift()
-// array.unshift(50);
-// console.log(`unshift() : => `+array);
+//13 delete
+delete array[1];
+console.log(`delete () : => `+array);
 
-// //12 index number
-// array[1] = 90;
-// console.log(`index number() : => `+array);
+//14 splice()
+array.splice(2,0,100,110);
+console.log(`splice () : => `+array);
 
-// //13 delete
-// delete array[1];
-// console.log(`delete () : => `+array);
-
-// //14 splice()
-// array.splice(2,0,100,110);
-// console.log(`splice () : => `+array);
-
-// //15 concat() 
-// const comman = y.concat(array);
-// console.log(`concat () : => `+comman);
+//15 concat() 
+const comman = y.concat(array);
+console.log(`concat () : => `+comman);
 
 //16 map()
 var x = array.map(x => x * 3);
@@ -79,7 +77,6 @@ console.log( `filter() `+filter);
 
 function checkfilter(array){
     return array>= 500;
-
 }
 
  //20 every()
@@ -91,5 +88,44 @@ function checkevery(array){
 
 }
 
+//21 fill()
+array.fill('abcd',7,9);
+console.log(`fill() =>`+array);
 
+//22 find()
+const ages = [6, 12, 18, 25];
+var s = ages.find(checkAge);
+console.log(`find() =>`+s);
+function checkAge(age){
+    return age>= 18;
+}
+//23 some
+var num = [11, 22, 33, 44];
+console.log(`some() =>`+num);
+var t = num.some(checknum);
+console.log(t);
+function checknum(age){
+    return age>= 18;
+}
+//24 valueOf()
+const fruit = ["Banana", "Orange", "Apple", "Mango"];
+fruit.valueOf();
+console.log(`valueOff()`+fruit);
 
+//25 toString()
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let text = fruits.toString();
+console.log(`toString()`+text);
+
+//26 reduce()
+const numbers = [175, 50, 25];
+console.log(`reduce()`+numbers.reduce(myFunc));
+function myFunc(total, num) {
+  return total - num;
+}
+
+//reduceRight()
+console.log(`reduceRight()`+numbers.reduceRight(myFunc));
+function myFunc(total, num) {
+  return total - num;
+}
--- task3/assets/tip_cal_script.js
+++ task3/assets/tip_cal_script.js
@@ -1,30 +1,43 @@
 const bill_Amt = [50,90,100,200,300] ;
-console.log(`BILL AMOUNT : => `+ bill_Amt);
 
-var tip_Amt = [];
-var total_bill = [];
 
-for(var i = 0; i < bill_Amt.length; i++){
-    
+const tip_Amt = [];
+const total_bill = [];
+
+const gst = (bill_Amt) => {
+for(let i = 0; i < bill_Amt.length; i++){ 
 if (bill_Amt[i] < 100){
-    //console.log(`AMOUNT is Less then 100 : => `+ bill_Amt[i]);
-
-    tip_Amt = (bill_Amt[i] * (15/100));
-   // console.log(`TIP AMOUNT : => `+ tip_Amt);
-
-    total_bill = tip_Amt + bill_Amt[i];
-    console.log(`Total AMOUNT : => `+ total_bill);
-}
-else{
-    //console.log(`AMOUNT is 100 or greater then 100 : => `+ bill_Amt[i]);
-
-    tip_Amt = (bill_Amt[i] * (20/100));
-    //console.log(`TIP AMOUNT : => `+ tip_Amt);
-    
-     total_bill = tip_Amt + bill_Amt[i];
-    console.log(`Total AMOUNT : => `+ total_bill);
+tip_Amt.push(bill_Amt[i] * (15/100));
+total_bill.push(bill_Amt[i] + (bill_Amt[i] * 0.15));
+}else{
+tip_Amt.push(bill_Amt[i] * (20/100));
+total_bill.push(bill_Amt[i] + (bill_Amt[i] * 0.2));
 }
 }
+}
+gst(bill_Amt);
+console.log(`BILL AMOUNT : => `+ bill_Amt);
+console.log(`Tip : => `+ tip_Amt);
 console.log(`TOTAl AMOUNT : => `+ total_bill);
 
 console.log(`=================================== `);
+
+
+// const billVal = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52, 50, 110];
+// const tip = [];
+// const total = [];
+// const calTip = (billVal) => {
+// for(let i = 0; i < billVal.length; i++){
+// if(billVal[i] >= 50 && billVal[i] <= 300){
+// tip.push(billVal[i] * 0.15);
+// total.push(billVal[i] + (billVal[i] * 0.15))
+// }else{
+// tip.push(billVal[i] * 0.20);
+// total.push(billVal[i] + (billVal[i] * 0.2))
+// }
+// }
+// }
+// calTip(billVal);
+// console.log('bill val', billVal)
+// console.log('calc tip', tip);
+// console.log('final bill', total);
