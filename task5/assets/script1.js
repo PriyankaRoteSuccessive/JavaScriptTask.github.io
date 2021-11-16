a7af8a8c8c9fd193e2965a4ab94a99555f92a996
PriyankaRoteSuccessive <priyanka.rote@successive.tech>
2021-11-16 18:25:08 -0500

    completed

---
+++ task5/assets/script1.js
@@ -0,0 +1,10 @@
+
+ let color = {
+    red : "#FF0000",
+    green : "#00FF00",
+    white : "#FFFFFF",
+ }
+let newArr = []
+newArr = Object.entries(color);
+console.log("users",newArr);
+console.log("=========================================================================")
---
+++ task5/assets/script3.js
@@ -0,0 +1,10 @@
+let  check = {
+    age : function(age){
+if(age>18){
+    console.log("Age above 18 is Major")
+}else{
+    console.log("Age below 18 is Minor")
+}
+}
+}
+console.log(check.age(20));
