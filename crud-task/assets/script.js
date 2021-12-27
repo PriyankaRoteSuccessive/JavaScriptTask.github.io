38ca5757c11aa9232debbe89702c751a64332516
PriyankaRoteSuccessive <priyanka.rote@successive.tech>
2021-12-27 17:35:09 +0530

    CRUD

---
+++ crud-task/assets/script.js
@@ -0,0 +1,244 @@
+showtask();
+let addtaskinput = document.getElementById("addtaskinput");
+let addtaskbtn = document.getElementById("addtaskbtn");
+
+addtaskbtn.addEventListener("click", function(){
+    addtaskinputval = addtaskinput.value;
+    if(addtaskinputval.trim()!=0){
+        let webtask = localStorage.getItem("localtask");
+        if(webtask == null){
+            taskObj = [];
+        }
+        else{
+            taskObj = JSON.parse(webtask);
+        }
+        taskObj.push({'task_name':addtaskinputval, 'completeStatus':false});
+		// console.log(taskObj, 'Ashendra');
+        localStorage.setItem("localtask", JSON.stringify(taskObj));
+        addtaskinput.value = '';
+    }
+    showtask();
+})
+
+// showtask
+function showtask(){
+    let webtask = localStorage.getItem("localtask");
+    if(webtask == null){
+        taskObj = [];
+    }
+    else{
+        taskObj = JSON.parse(webtask);
+    }
+    let html = '';
+    let addedtasklist = document.getElementById("addedtasklist");
+    taskObj.forEach((item, index) => {
+
+        if(item.completeStatus==true){
+            taskCompleteValue = `<td class="completed">${item.task_name}</td>`;
+        }else{
+            taskCompleteValue = `<td>${item.task_name}</td>`;
+        }
+        html += `<tr>
+                    <th scope="row">${index+1}</th>
+                    ${taskCompleteValue}
+                    <td><button type="button" onclick="edittask(${index})" class="text-primary"><i class="fa fa-edit"></i>Edit</button></td>
+                    <td><button type="button" onclick="deleteitem(${index})" class="text-danger"><i class="fa fa-trash"></i>Delete</button></td>
+                </tr>`;
+    });
+    addedtasklist.innerHTML = html;
+}
+
+// edittask
+function edittask(index){
+    let saveindex = document.getElementById("saveindex");
+    let addtaskbtn = document.getElementById("addtaskbtn");
+    let savetaskbtn = document.getElementById("savetaskbtn");
+    saveindex.value = index;
+    let webtask = localStorage.getItem("localtask");
+    let taskObj = JSON.parse(webtask); 
+    
+    addtaskinput.value = taskObj[index]['task_name'];
+    addtaskbtn.style.display="none";
+    savetaskbtn.style.display="block";
+}
+
+// savetask
+let savetaskbtn = document.getElementById("savetaskbtn");
+savetaskbtn.addEventListener("click", function(){
+    let addtaskbtn = document.getElementById("addtaskbtn");
+    let webtask = localStorage.getItem("localtask");
+    let taskObj = JSON.parse(webtask); 
+    let saveindex = document.getElementById("saveindex").value;
+    
+    for (keys in taskObj[saveindex]) {
+        if(keys == 'task_name'){
+            taskObj[saveindex].task_name = addtaskinput.value;
+        }
+      }
+    // taskObj[saveindex] = {'task_name':addtaskinput.value, 'completeStatus':false} ;
+  //  taskObj[saveindex][task_name] = addtaskinput.value;
+    savetaskbtn.style.display="none";
+    addtaskbtn.style.display="block";
+    localStorage.setItem("localtask", JSON.stringify(taskObj));
+    addtaskinput.value='';
+    showtask();
+})
+// deleteitem
+function deleteitem(index){
+    let webtask = localStorage.getItem("localtask");
+    let taskObj = JSON.parse(webtask);
+    taskObj.splice(index, 1);
+    localStorage.setItem("localtask", JSON.stringify(taskObj));
+    showtask();
+}
+
+// complete task
+let addedtasklist = document.getElementById("addedtasklist");
+    addedtasklist.addEventListener("click", function(e){
+       // console.log(e);
+        
+        // showtask();
+        let webtask = localStorage.getItem("localtask");
+        let taskObj = JSON.parse(webtask);
+        
+        let mytarget = e.target;
+        if(mytarget.classList[0] === 'text-success'){
+        let mytargetid = mytarget.getAttribute("id");
+        
+        
+        // let taskValue = taskObj[mytargetid]['task_name'];
+        
+        mytargetpresibling = mytarget.parentElement.previousElementSibling.previousElementSibling;
+            
+            // let mynewelem = mytargetpresibling.classList.toggle("completed");
+            // taskObj.splice(mytargetid,1,mynewelem);
+            for (keys in taskObj[mytargetid]) {
+                if(keys == 'completeStatus' && taskObj[mytargetid][keys]==true){
+                    taskObj[mytargetid].completeStatus = false;
+                   // taskObj[mytargetid] = {'task_name':taskValue, 'completeStatus':false};
+                }else if(keys == 'completeStatus' && taskObj[mytargetid][keys]==false){
+                    taskObj[mytargetid].completeStatus = true;
+                    //taskObj[mytargetid] = {'task_name':taskValue, 'completeStatus':true};
+                }
+              }
+        //}
+       // showtask();        
+        localStorage.setItem("localtask", JSON.stringify(taskObj));
+        showtask();
+    }
+    })
+
+// deleteall
+let deleteallbtn = document.getElementById("deleteallbtn");
+deleteallbtn.addEventListener("click", function(){
+    let savetaskbtn = document.getElementById("savetaskbtn");
+    let addtaskbtn = document.getElementById("addtaskbtn");
+    let webtask = localStorage.getItem("localtask");
+    let taskObj = JSON.parse(webtask);
+    if(webtask == null){
+        taskObj = [];
+    }
+    else{
+        taskObj = JSON.parse(webtask);
+        taskObj = [];
+    }
+    savetaskbtn.style.display="none";
+    addtaskbtn.style.display="block";
+    localStorage.setItem("localtask", JSON.stringify(taskObj));
+    showtask();
+})
+
+
+//pagination
+var tableData = [taskObj];
+var state = {
+    'querySet': tableData,
+
+    'page': 1,
+    'rows': 5,
+    'window': 5,
+}
+
+buildTable()
+
+function pagination(querySet, page, rows) {
+
+    var trimStart = (page - 1) * rows
+    var trimEnd = trimStart + rows
+
+    var trimmedData = querySet.slice(trimStart, trimEnd)
+
+    var pages = Math.round(querySet.length / rows);
+
+    return {
+        'querySet': trimmedData,
+        'pages': pages,
+    }
+}
+
+function pageButtons(pages) {
+    var wrapper = document.getElementById('pagination-wrapper')
+
+    wrapper.innerHTML = ``
+	console.log('Pages:', pages)
+
+    var maxLeft = (state.page - Math.floor(state.window / 2))
+    var maxRight = (state.page + Math.floor(state.window / 2))
+
+    if (maxLeft < 1) {
+        maxLeft = 1
+        maxRight = state.window
+    }
+
+    if (maxRight > pages) {
+        maxLeft = pages - (state.window - 1)
+        
+        if (maxLeft < 1){
+        	maxLeft = 1
+        }
+        maxRight = pages
+    }
+    
+    
+
+    for (var page = maxLeft; page <= maxRight; page++) {
+    	wrapper.innerHTML += `<button value=${page} class="page btn btn-sm btn-info">${page}</button>`
+    }
+
+    if (state.page != 1) {
+        wrapper.innerHTML = `<button value=${1} class="page btn btn-sm btn-info">&#171; First</button>` + wrapper.innerHTML
+    }
+
+    if (state.page != pages) {
+        wrapper.innerHTML += `<button value=${pages} class="page btn btn-sm btn-info">Last &#187;</button>`
+    }
+
+    $('.page').on('click', function() {
+        $('#table-body').empty()
+
+        state.page = Number($(this).val())
+
+        buildTable()
+    })
+
+}
+
+
+function buildTable() {
+    var table = $('#table-body')
+
+    var data = pagination(state.querySet, state.page, state.rows)
+    var myList = data.querySet
+
+    for (var i = 1 in myList) {
+        //Keep in mind we are using "Template Litterals to create rows"
+        var row = `<tr>
+                  <td>${myList[i].rank}</td>
+                  <td>${myList[i].first_name}</td>
+                  <td>${myList[i].last_name}</td>
+                  `
+        table.append(row)
+    }
+
+    pageButtons(data.pages)
+}
